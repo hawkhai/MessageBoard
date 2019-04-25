965c0d03b8106bd32585bf49a4d87f383c80b701
gtfly <37724890+gtfly@users.noreply.github.com>
2019-04-25 20:23:02 +0800

    Add files via upload

---
+++ login_and_register/login.php
@@ -0,0 +1,38 @@
+<?php
+$vcode = @file_get_contents('vcode.txt');
+
+$input = @$_POST['vcode'];
+$username = @$_POST['username'];
+$password = @$_POST['password'];
+
+if($input === $vcode && $username && $password)
+{
+	include('sql.php');
+	$login = new Mysql();
+	$login->username = $username;
+	$login->password = $password;
+	
+	if($login->Query())
+	{
+		echo 'welcome ' . $username;
+	}
+	
+	else
+	{
+		die('username or password is wrong! please <a href="login.html"> login again! </a>');
+	}
+
+}
+elseif ($input !== $vcode) {
+	echo 'vcode error!';
+	echo "
+		<a href='login.html'>login again</a>
+	";
+}
+
+else{
+	echo 'username or password is null!';
+	echo "
+		<a href='login.html'>login again</a>
+	";
+}
